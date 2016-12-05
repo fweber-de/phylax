b46ab1ae2b130a6d308f73dc0208d7f57200ff69
Florian Weber <fweber@ligneus.de>
2016-12-05 12:07:22 +0100

    nav

---
+++ src/AppBundle/Twig/StringExtension.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\Twig;
+
+/**
+ * @author Florian Weber <florian.weber@fweber.info>
+ */
+class StringExtension extends \Twig_Extension
+{
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter('format_trace', array($this, 'formatTraceFilter')),
+            new \Twig_SimpleFilter('gravatarHash', array($this, 'gravatarHashFilter')),
+        );
+    }
+
+    public function formatTraceFilter($trace)
+    {
+        return str_replace('#', '<br>#', $trace);
+    }
+
+    public function gravatarHashFilter($email)
+    {
+        return md5(strtolower(trim($email)));
+    }
+
+    public function getName()
+    {
+        return 'string_extension';
+    }
+}
